ccf70f657dfcdc891e059f4f42fa6f1150cfe094
tunji <88969825+tunny17@users.noreply.github.com>
2022-06-23 12:07:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let name = document.getElementById('name').innerHTML = "Name : Adeyemi Adetunji."
+
+let height = document.getElementById('height').innerHTML = "Height : 6 foot 2 inches."
+
+let country = document.getElementById('country').innerHTML = "Country : Nigeria.";
